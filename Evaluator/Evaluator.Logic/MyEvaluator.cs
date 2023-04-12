8f7d53fd9478f7af2704f198473180b6ae61bd36
Luis Florez <braian1k3@gmail.com>
2023-04-12 17:21:03 -0500

    Execute.

--- Evaluator/Evaluator.Logic/MyEvaluator.cs
+++ Evaluator/Evaluator.Logic/MyEvaluator.cs
@@ -1,4 +1,8 @@
-﻿namespace Evaluator.Logic
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Linq.Expressions;
+
+namespace Evaluator.Logic
 {
     public class MyEvaluator
     {
@@ -15,9 +19,9 @@
             {
                 if (IsOperator(postfix[i]))
                 {
-                    var number2 = stack.Pop();
-                    var number1 = stack.Pop();
-                    var result = Calculate(number1, postfix[i], number2);
+                    double number2 = stack.Pop();
+                    double number1 = stack.Pop();
+                    double result = Calculate(number1, postfix[i], number2);
                     stack.Push(result);
                 }
                 else
@@ -34,7 +38,6 @@
                         }
                         else
                         {
-                            i++;
                             break;
                         }
                     }
@@ -43,12 +46,34 @@
                     {
                         figure += postfix[j];
                     }
-                    var number = (double)Convert.ToDouble(figure);
+                    double number = ValidateSpots(figure);
                     stack.Push(number);
-                    i--;
                 }
             }
             return stack.Pop();
+        }
+
+        private static double ValidateSpots(string figure)
+        {
+            bool validation = false;
+            int locate = 0;
+            for (int i = 0; i < figure.Length; i++)
+            {
+                if (figure[i] == '.')
+                {
+                    validation = true;
+                    locate = i;
+                    break;
+                }
+            }
+            if (validation)
+            {
+                return Convert.ToDouble(figure) / Math.Pow(10, (figure.Length - 1 - locate));
+            }
+            else
+            {
+                return Convert.ToDouble(figure);
+            }
         }
 
         private static double Calculate(double number1, char @operator, double number2)
@@ -60,7 +85,7 @@
                 case '/': return number1 / number2;
                 case '+': return number1 + number2;
                 case '-': return number1 - number2;
-                default: throw new Exception("Not valid operator");
+                default: throw new Exception("Expression is not valid.");
             }
         }
 
@@ -68,10 +93,39 @@
         {
             var stack = new Stack<char>(100);
             var postfix = string.Empty;
+            ValidateBrackets(infix);
+            ValidatePossibleOperatorsExtremes(infix[0], infix[infix.Length - 1]);
             for (int i = 0; i < infix.Length; i++)
             {
-                if (IsOperator(infix[i]))
+                if (IsOperator(infix[i]))   
                 {
+                    bool changeOperator = false;
+                    if (i != infix.Length - 1)
+                    {
+                        if (infix[i] == '(')
+                        {
+                            ValidatePossibleNextOperator(infix[i + 1]);
+                        }
+                        else
+                        {
+                            if (infix[i] == ')')
+                            {
+                                ValidatePossibleNextBracket(infix[i + 1]);
+                            }
+                            else
+                            {
+                                if (i != infix.Length - 2)
+                                {
+                                    ValidateOperators(infix[i + 1], infix[i + 2]);
+                                    changeOperator = ValidateOperators(infix[i + 1]);
+                                }
+                            }
+                        }
+                    }
+                    if (changeOperator == true)
+                    {
+                        i++;
+                    }
                     if (stack.IsEmpty)
                     {
                         stack.Push(infix[i]);
@@ -104,31 +158,50 @@
                 {
                     if (i != infix.Length - 1) 
                     {
-                        int index = i;
-                        int lenght = 1;
-                        i++;
-                        while (infix[i] != '(' && infix[i] != ')' && infix[i] != '^' && infix[i] != '/' && infix[i] != '*' && infix[i] != '+' && infix[i] != '-')
+                        if (infix[i] != '.')
                         {
-                            if (i == infix.Length - 1)
+                            int index = i;
+                            int lenght = 1;
+                            i++;
+                            ValidateSpots(infix, i);
+                            while (infix[i] != '(' && infix[i] != ')' && infix[i] != '^' && infix[i] != '/' && infix[i] != '*' && infix[i] != '+' && infix[i] != '-')
                             {
+                                if (i == infix.Length - 1)
+                                {
+                                    lenght++;
+                                    i++;
+                                    break;
+                                }
                                 lenght++;
                                 i++;
-                                break;
                             }
-                            lenght++;
-                            i++;
+                            for (int j = index; j < lenght + index; j++)
+                            {
+                                postfix += infix[j];
+                            }
+                            if (postfix[postfix.Length - 1] == '.')
+                            {
+                                throw new Exception("Expression is not valid.");
+                            }
+                            postfix += 'N';
+                            i--;
                         }
-                        for (int j = index; j < lenght + index; j++)
+                        else
                         {
-                            postfix += infix[j];
+                            throw new Exception("Expression is not valid.");
                         }
-                        postfix += 'N';
-                        i--;
                     }
                     else
                     {
-                        postfix += infix[i];
-                        postfix += 'N';
+                        if (infix[i] != '.')
+                        {
+                            postfix += infix[i];
+                            postfix += 'N';
+                        }
+                        else
+                        {
+                            throw new Exception("Expression is not valid.");
+                        }
                     }
                 }
             }
@@ -139,9 +212,102 @@
             return postfix;
         }
 
+        private static void ValidateBrackets(string infix)
+        {
+            int openBracket = 0, closedBracket = 0;
+            for (int i = 0; i < infix.Length; i++)
+            {
+                if (infix[i] == '(')
+                {
+                    openBracket++;
+                }
+                else
+                {
+                    if (infix[i] == ')')
+                    {
+                        closedBracket++;
+                    }
+                }
+            }
+            if (openBracket != closedBracket)
+            {
+                throw new Exception("Expression is not valid.");
+            }
+        }
+
+        private static void ValidatePossibleOperatorsExtremes(char item1, char item2)
+        {
+            if (item1 == '^' || item1 == '/' || item1 == '*' || item1 == '+' || item1 == '-')
+            {
+                throw new Exception("Expression is not valid.");
+            }
+            if (item2 == '^' || item2 == '/' || item2 == '*' || item2 == '+' || item2 == '-')
+            {
+                throw new Exception("Expression is not valid.");
+            }
+        }
+
         private static bool IsOperator(char item)
         {
             if (item == '(' || item == ')' || item == '^' || item == '/' || item == '*' || item == '+' || item == '-')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static void ValidatePossibleNextOperator(char item)
+        {
+            if (item == '^' || item == '/' || item == '*' || item == '+' || item == '-')
+            {
+                throw new Exception("Expression is not valid.");
+            }
+        }
+
+        private static void ValidatePossibleNextBracket(char item)
+        {
+            if (item != ')' && item != '^' && item != '/' && item != '*' && item != '+' && item != '-')
+            {
+                throw new Exception("Expression is not valid.");
+            }
+        }
+
+        private static void ValidateOperators(char item1, char item2)
+        {
+            if (IsOperator(item1, item2))
+            {
+                throw new Exception("Expression is not valid.");
+            }
+        }
+
+        private static bool IsOperator(char item1, char item2)
+        {
+            if (item1 == '^' || item1 == '/' || item1 == '*' || item1 == '+' || item1 == '-')
+            {
+                if (item2 == '^' || item2 == '/' || item2 == '*' || item2 == '+' || item2 == '-')
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        private static bool ValidateOperators(char item)
+        {
+            if (IsOperatorArithmetic(item))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsOperatorArithmetic(char item)
+        {
+            if (item == '^' || item == '/' || item == '*' || item == '+' || item == '-')
             {
                 return true;
             }
@@ -158,7 +324,7 @@
                 case '+': return 1;
                 case '-': return 1;
                 case '(': return 5;
-                default: throw new Exception("Not valid operator");
+                default: throw new Exception("Expression is not valid.");
             }
         }
 
@@ -172,7 +338,36 @@
                 case '+': return 1;
                 case '-': return 1;
                 case '(': return 0;
-                default: throw new Exception("Not valid operator");
+                default: throw new Exception("Expression is not valid.");
+            }
+        }
+
+        private static void ValidateSpots(string infix, int i)
+        {
+            int countSpot = 0;
+
+            while (infix[i] != '(' && infix[i] != ')' && infix[i] != '^' && infix[i] != '/' && infix[i] != '*' && infix[i] != '+' && infix[i] != '-')
+            {
+                if (i == infix.Length - 1)
+                {
+                    if (infix[i] == '.')
+                    {
+                        throw new Exception("Expression is not valid.");
+                    }
+                    break;
+                }
+                else
+                {
+                    if (infix[i] == '.')
+                    {
+                        countSpot++;
+                    }
+                    i++;
+                }
+            }
+            if (countSpot > 1)
+            {
+                throw new Exception("Expression is not valid.");
             }
         }
     }
--- Evaluator/Evaluator.UI.Console/Program.cs
+++ Evaluator/Evaluator.UI.Console/Program.cs
@@ -1,9 +1,14 @@
 ﻿using Evaluator.Logic;
 
-Console.WriteLine("Expresions Evaluator");
-var expresion1 = "4*5/(4+6)";
-var expresion2 = "4*(5+6-(8/2^3)-7)-1";
-var expresion3 = "30+40*(2.33/0.48)*7";
-Console.WriteLine($"{expresion1} = {MyEvaluator.Evaluate(expresion1)}");
-Console.WriteLine($"{expresion2} = {MyEvaluator.Evaluate(expresion2)}");
-Console.WriteLine($"{expresion3} = {MyEvaluator.Evaluate(expresion3)}");
+try
+{
+    Console.WriteLine("Expresions Evaluator");
+    var expresion1 = "4*5/(4+6)";
+    var expresion2 = "4*(5+6-(8/2^3)-7)-1";
+    Console.WriteLine($"{expresion1} = {MyEvaluator.Evaluate(expresion1)}");
+    Console.WriteLine($"{expresion2} = {MyEvaluator.Evaluate(expresion2)}");
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
